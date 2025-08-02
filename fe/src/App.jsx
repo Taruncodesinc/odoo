95828edf097ec62e484d0427d32175211ca021a6
Spyder-v <rishabhawasthi7866@gmail.com>
2025-08-02 15:01:58 +0530

    Changed Landing

--- fe/src/App.jsx
+++ fe/src/App.jsx
@@ -1,12 +1,29 @@
-
+import React from "react";
 import './App.css'
+import { BrowserRouter as Router,Routes,Route } from "react-router-dom";
+
+import Landing from "../Pages/Landing";
+import Home from "../Pages/Home";
+import Issues from "../Pages/Issues";
+import Login from "../Pages/Login";
+import Sign from "../Pages/Sign";
 
 function App() {
 
 
   return (
     <>
-     <div className='bg-red-500'>Hello</div>
+    <Router>
+      <Routes>
+      <Route path="/" element={<Landing />} />
+      <Route path="/login" element={<Login />} />
+      <Route path="/Sign" element={<Sign />} />
+
+      {/* Protected DashBoard Routes */}
+      <Route path="/home" element={<Home />} />
+      <Route path="Issues" element={<Issues />} />
+      </Routes>
+    </Router>
     </>
   )
 }
