9d653b17e833da2b242f692c3f62b4c324e30057
Spyder-v <rishabhawasthi7866@gmail.com>
2025-08-02 15:16:35 +0530

    Created landing page

--- fe/src/App.jsx
+++ fe/src/App.jsx
@@ -1,9 +1,4 @@
-<<<<<<< HEAD
-
-import Login from '../Pages/Login'
-=======
 import React from "react";
->>>>>>> 95828edf097ec62e484d0427d32175211ca021a6
 import './App.css'
 import { BrowserRouter as Router,Routes,Route } from "react-router-dom";
 
@@ -18,10 +13,6 @@ function App() {
 
   return (
     <>
-<<<<<<< HEAD
-     <div className='bg-red-500'>Hello</div>
-     <Login/>
-=======
     <Router>
       <Routes>
       <Route path="/" element={<Landing />} />
@@ -33,7 +24,6 @@ function App() {
       <Route path="Issues" element={<Issues />} />
       </Routes>
     </Router>
->>>>>>> 95828edf097ec62e484d0427d32175211ca021a6
     </>
   )
 }
